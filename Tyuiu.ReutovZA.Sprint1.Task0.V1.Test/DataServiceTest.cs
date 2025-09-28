c710aaf00529c1759541b4129f6afd9ffa24e599
Захар Реутов <reutovz809@gmail.com>
2025-09-28 14:41:49 +0500

    Add project files.

---
+++ Tyuiu.ReutovZA.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ReutovZA.Sprint1.Task0.V1.Lib;
+
+namespace Tyuiu.ReutovZA.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
